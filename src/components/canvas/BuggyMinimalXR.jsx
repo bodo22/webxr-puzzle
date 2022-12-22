430382f75108e61af3fb3ef16cb733976dd00682
Wilhelm Herbrich <wilhelm.herbrich@st.ovgu.de>
2022-12-22 10:38:58 +0100

    chore: cleanup, add possible fix

--- src/components/canvas/BuggyMinimalXR.jsx
+++ src/components/canvas/BuggyMinimalXR.jsx
@@ -1,159 +1,29 @@
 import * as React from 'react'
 import * as THREE from 'three'
-import create, { EqualityChecker, GetState, SetState, StateSelector, UseBoundStore } from 'zustand'
+import create from 'zustand'
 import { useThree } from '@react-three/fiber'
-import { useIsomorphicLayoutEffect } from '@react-three/xr'
 
-const colors = { left: 0xff0000, right: 0x0000ff }
-
-class TestEvents {
-  constructor(wrapper) {
-    this.wrapper = wrapper
-    return this
-  }
-
-  async wait(waitTime) {
-    await new Promise((resolve) => setTimeout(resolve, waitTime))
-    return this
-  }
-
-  async event(event) {
-    this.wrapper.dispatchEvent(event)
-    return this
-  }
-}
-
-const events = {
-  left: [
-    { wait: 500 },
-    { type: 'connected' },
-    // { wait: 2000 },
-    // { type: 'disconnected' },
-    // // { wait: 1 },
-    // { type: 'connected' },
-    // { wait: 1000 },
-    // { type: 'disconnected' },
-    // { type: 'connected' },
-    // { wait: 1000 },
-    // { type: 'disconnected' },
-    // { type: 'connected' },
-  ],
-  right: [
-    { wait: 500 },
-    { type: 'connected' },
-    // { wait: 1000 },
-    // { type: 'disconnected' },
-    // { type: 'connected' },
-    // { wait: 1000 },
-    // { wait: 1000 },
-    // { type: 'disconnected' },
-    // { type: 'connected' },
-    // { wait: 1000 },
-    // { type: 'disconnected' },
-    // { type: 'connected' },
-  ],
-}
-
-class Box extends THREE.Mesh {
-  constructor(boxGeo, material, index) {
-    super(boxGeo, material)
-    this.index = index
-
-    // setInterval(() => {
-    //   setTimeout(() => {
-    //     this.dispatchEvent({ type: 'connected' })
-    //     setTimeout(() => {
-    //       this.dispatchEvent({ type: 'disconnected' })
-    //     }, 1000 + this.index * 50)
-    //   }, 1000 + this.index * 50)
-    // }, 3000 + 1000 * this.index)
-  }
-
-  async startEvents() {
-    const testEvents = new TestEvents(this)
-
-    for (const event of events[this.index]) {
-      //   console.log(this.index, event)
-      if (event.wait) {
-        await testEvents.wait(event.wait)
-      } else {
-        await testEvents.event(event)
-      }
-    }
-  }
-}
-
+const colors = { red: 0xff0000, 0: 0xff0000, blue: 0x0000ff, 1: 0x0000ff }
 class FakeXRController extends THREE.Group {
   constructor(index, gl) {
     super()
-
-    this.color = `color: rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(
-      Math.random() * 255,
-    )})`
 
     this.index = index
     this.material = new THREE.MeshBasicMaterial({ color: colors[this.index] })
 
     this.boxGeo = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5)
-    this.box = new Box(this.boxGeo, this.material, this.index)
-    this.box.position.setY(-3)
-
-    this.coneGeo = new THREE.ConeBufferGeometry(0.5, 0.5, 10)
-    this.cone = new THREE.Mesh(this.coneGeo, this.material)
-    this.cone.position.setY(0)
-
-    this.icosahedronGeo = new THREE.IcosahedronBufferGeometry(0.5)
-    this.icosahedron = new THREE.Mesh(this.icosahedronGeo, this.material)
-    this.icosahedron.position.setY(3)
+    this.box = new THREE.Mesh(this.boxGeo, this.material)
 
     this.position.setZ(-5)
-    this.position.setX(this.index === 'left' ? -2 : 2)
+    this.position.setX(this.index === 'red' || this.index === 0 ? -2 : 2)
 
-    this.cone.userData.name = 'grip'
-    this.box.userData.name = 'controller'
-    this.icosahedron.userData.name = 'hand'
-
-    this.visible = false
-    this.add(this.box, this.cone, this.icosahedron)
-
-    this._onConnected = this._onConnected.bind(this)
-    this._onDisconnected = this._onDisconnected.bind(this)
-
-    this.box.addEventListener('connected', this._onConnected)
-    this.box.addEventListener('disconnected', this._onDisconnected)
+    this.add(this.box)
 
     this._addedCallback = this._addedCallback.bind(this)
     this._removedCallback = this._removedCallback.bind(this)
-    // this._addedCallbackHand = this._addedCallbackHand.bind(this)
-    // this._removedCallbackHand = this._removedCallbackHand.bind(this)
-    // this._addedCallbackGrip = this._addedCallbackGrip.bind(this)
-    // this._removedCallbackGrip = this._removedCallbackGrip.bind(this)
-    // this._addedCallbackController = this._addedCallbackController.bind(this)
-    // this._removedCallbackController = this._removedCallbackController.bind(this)
 
     this.addEventListener('added', this._addedCallback)
     this.addEventListener('removed', this._removedCallback)
-
-    // this.box.addEventListener('added', this._addedCallbackHand)
-    // this.box.addEventListener('removed', this._removedCallbackHand)
-
-    // this.conoe.addEventListener('added', this._addedCallbackGrip)
-    // this.conoe.addEventListener('removed', this._removedCallbackGrip)
-
-    // this.icosahedron.addEventListener('added', this._addedCallbackController)
-    // this.icosahedron.addEventListener('removed', this._removedCallbackController)
-
-    this.box.startEvents()
-  }
-
-  _onConnected(event) {
-    this.visible = true
-    this.dispatchEvent(event)
-  }
-
-  _onDisconnected(event) {
-    this.visible = false
-    this.dispatchEvent(event)
   }
 
   _addedCallback(e) {
@@ -168,37 +38,9 @@ class FakeXRController extends THREE.Group {
     console.trace()
   }
 
-  //   _addedCallbackHand(e) {
-  //     console.log(`%c added hand ${this.index}`, this.color)
-  //   }
-
-  //   _removedCallbackHand(e) {
-  //     console.log(`%c removed hand ${this.index}`, this.color)
-  //   }
-
-  //   _addedCallbackGrip(e) {
-  //     console.log(`%c added grip ${this.index}`, this.color)
-  //   }
-
-  //   _removedCallbackGrip(e) {
-  //     console.log(`%c removed grip ${this.index}`, this.color)
-  //   }
-
-  //   _addedCallbackController(e) {
-  //     console.log(`%c added controller ${this.index}`, this.color)
-  //   }
-
-  //   _removedCallbackController(e) {
-  //     console.log(`%c removed controller ${this.index}`, this.color)
-  //     // console.trace()
-  //   }
-
   dispose() {
     this.removeEventListener('added', this._addedCallback)
     this.removeEventListener('removed', this._removedCallback)
-
-    this.box.removeEventListener('connected', this._onConnected)
-    this.box.removeEventListener('disconnected', this._onDisconnected)
   }
 }
 
@@ -217,38 +59,22 @@ export default function BuggyMinimalXR({ children }) {
   const controllers = store((state) => state.controllers)
 
   React.useLayoutEffect(() => {
-    const handlers = ['left', 'right'].map((id) => {
+    ;[0, 1].forEach((id) => {
       const target = new FakeXRController(id, gl)
-      const onConnected = () =>
-        set((state) => {
-          console.log('onConnected set', id)
-          return { controllers: [...state.controllers, target] }
-        })
-      const onDisconnected = () =>
-        set((state) => {
-          console.log('onDisconnected set', id)
-          return { controllers: state.controllers.filter((it) => it !== target) }
-        })
-
-      target.addEventListener('connected', onConnected)
-      target.addEventListener('disconnected', onDisconnected)
-
-      return () => {
-        target.removeEventListener('connected', onConnected)
-        target.removeEventListener('disconnected', onDisconnected)
-      }
+      set((state) => {
+        return { controllers: [...state.controllers, target] }
+      })
     })
-
-    return () => handlers.forEach((cleanup) => cleanup())
   }, [gl, set])
 
   console.log(
-    'controllers',
+    'new controllers index',
     controllers.map(({ index }) => index),
   )
+
   React.useLayoutEffect(() => {
     console.log(
-      'scene.children',
+      'scene.children in useLayoutEffect',
       scene.children.map(({ index }) => index),
     )
   }, [controllers, scene])
@@ -256,8 +82,8 @@ export default function BuggyMinimalXR({ children }) {
   React.useLayoutEffect(() => {
     let to = setTimeout(() => {
       set((state) => {
-        console.log(state.controllers)
-        return { controllers: state.controllers.reverse() }
+        console.log('swapping controllers!!!')
+        return { controllers: [...state.controllers.reverse()] }
       })
     }, 1000)
     return () => {
@@ -269,7 +95,7 @@ export default function BuggyMinimalXR({ children }) {
   return (
     <>
       {controllers.map((controller, i) => (
-        <primitive key={i} object={controller} />
+        <primitive key={controller.index} object={controller} />
       ))}
       {children}
     </>
