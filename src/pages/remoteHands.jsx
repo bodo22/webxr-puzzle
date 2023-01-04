1e502cec5f57d2678e78093c17fe2bdc5b455ede
Wilhelm Herbrich <wilhelm.herbrich@st.ovgu.de>
2023-01-04 15:55:08 +0100

    feat: add prototype transient position change via zustand

--- src/pages/remoteHands.jsx
+++ src/pages/remoteHands.jsx
@@ -1,5 +1,7 @@
-import RemoteHands from '@/components/canvas/RemoteHands'
+import React from 'react'
 import { MeshDistortMaterial } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import useRemoteHands from '@/stores/remoteHands'
 
 export default function Page(props) {
   return (
@@ -11,19 +13,56 @@ export default function Page(props) {
   )
 }
 
-Page.canvas = (props) => {
+const Cube = (props) => {
+  const ref = React.useRef()
+  // const coords = useRemoteHands((state) => state[props.id])
+
+  React.useEffect(() => {
+    const unsubscribe = useRemoteHands.subscribe(
+      (state) => state[props.id],
+      (coords, prevCoords) => {
+        if (!ref.current) {
+          console.log(coords, prevCoords)
+          return
+        }
+        ref.current.position.x = coords[0]
+        ref.current.position.y = coords[1]
+      },
+    )
+
+    return () => {
+      unsubscribe()
+    }
+  }, [props.id])
+  console.log('rerendering', props.id)
+
   return (
     <mesh
+      ref={ref}
       position={[0, 0, -2]}
       onClick={console.log}
       // onPointerOver={() => hover(true)}
       // onPointerOut={() => hover(false)}
-      {...props}>
+    >
       <sphereGeometry args={[1, 64, 64]} />
-      <MeshDistortMaterial roughness={0} color={false ? 'hotpink' : '#1fb2f5'} />
+      <MeshDistortMaterial roughness={0} color={props.id ? 'hotpink' : '#1fb2f5'} />
     </mesh>
   )
 }
+
+const Cubes = (props) => {
+  const items = useRemoteHands((state) => state.items)
+
+  useFrame((state, delta, xrFrame) => {
+    useRemoteHands.getState().advance()
+  })
+
+  return items.map((item) => {
+    return <Cube key={item} id={item} />
+  })
+}
+
+Page.canvas = (props) => <Cubes {...props} />
 
 export async function getStaticProps() {
   return { props: { title: 'RemoteHands' } }
