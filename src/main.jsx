f9b08d9f113fb772e8808f4e91e1cc7b076f105e
Wilhelm Herbrich <wilhelm.herbrich@st.ovgu.de>
2023-03-17 08:50:53 +0100

    feat: add support for remote devtools on hololens via chii

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// timeout needed when debugging holelens
+// reason: the only hololens devtools access over the network i found
+// currently works with chii, which needs to load first and initialize
+// to monitor requests the app makes 
+// setTimeout(async () => {
+const React = await import("react");
+const ReactDOM = await import("react-dom/client");
+const { default: App } = await import("./App");
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+// }, 5000);
